858237ed05f62dee7acc9f981499f1deabb9f1eb
Hanane <mezhanane76@gmail.com>
2026-01-06 14:37:49 +0100

    filtrage et details

--- src/Repository/ItemRepository.php
+++ src/Repository/ItemRepository.php
@@ -16,6 +16,38 @@ class ItemRepository extends ServiceEntityRepository
         parent::__construct($registry, Item::class);
     }
 
+
+
+      public function findWithSearch(array $criteria)
+    {
+        $qb = $this->createQueryBuilder('i')
+                   ->orderBy('i.dateLost', 'DESC'); // On trie du plus récent au plus vieux
+
+        // 1. Recherche par Mots Clés (Titre ou Description ou Mots clés)
+        // On ne l'ajoute QUE si l'utilisateur a écrit quelque chose
+        if (!empty($criteria['keywords'])) {
+            $qb->andWhere('i.title LIKE :keywords OR i.description LIKE :keywords OR i.keywords LIKE :keywords')
+               ->setParameter('keywords', '%' . $criteria['keywords'] . '%');
+        }
+
+        // 2. Recherche par Ville
+        // On ne l'ajoute QUE si l'utilisateur a écrit une ville
+        if (!empty($criteria['city'])) {
+            $qb->andWhere('i.city LIKE :city')
+               ->setParameter('city', '%' . $criteria['city'] . '%');
+        }
+
+        // 3. Recherche par Catégorie
+        // On ne l'ajoute QUE si l'utilisateur a choisi une catégorie
+        if (!empty($criteria['category'])) {
+            $qb->andWhere('i.category = :category')
+               ->setParameter('category', $criteria['category']);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+}
+
 //    /**
 //     * @return Item[] Returns an array of Item objects
 //     */
@@ -40,4 +72,4 @@ class ItemRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
-}
+
